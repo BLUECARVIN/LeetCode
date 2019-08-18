27c3a4466c106ca7ee2d247d4119b78a09ac682f
Bluecarvin <bluecarvin@163.com>
2019-08-18 18:28:04 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        label = {}
+        ans = []
+        for i in range(len(nums)):
+            if target - nums[i] in label:
+                ans = [label[target-nums[i]], i]
+            else:
+                label[nums[i]] = i
+        return ans
+# ----------- 76ms, 15.7MB ---------- #
