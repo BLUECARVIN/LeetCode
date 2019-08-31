d55fbff4f782ae3d2718f32aa2e296efb329b4f9
Bluecarvin <bluecarvin@163.com>
2019-08-31 21:40:50 +0800

    Create 141.py

---
+++ 141.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        p = head
+        q = head
+        while p != None and q != None and q.next != None:
+            p = p.next
+            q = q.next
+            q = q.next
+            if p == q:
+                return True
+        return False
+        
+# ---------- 40ms, 18.1MB ---------- #
